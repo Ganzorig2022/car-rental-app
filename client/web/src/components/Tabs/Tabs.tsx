e650598d110700d27b54d47a9d366b13f17cf64a
Temuujin Erdenebileg <102133245+temuujin2@users.noreply.github.com>
2023-05-03 01:14:57 +0800

    about us

--- client/web/src/components/Tabs/Tabs.tsx
+++ client/web/src/components/Tabs/Tabs.tsx
@@ -1,73 +1,202 @@
+import Image from "next/image";
 import { useState } from "react";
 
 function Tabs() {
-  const [toggleState, setToggleState] = useState(1);
+  const [show, setShow] = useState(true);
 
-  const toggleTab = (index) => {
-    setToggleState(index);
+  const handleClick = () => {
+    setShow((show) => !show);
   };
 
+  const btnArray = [
+    {
+      id: 1,
+      image: "/icons/engine_about.png",
+      text: "Engine",
+      checkIcon: "/icons/check-orange.png",
+    },
+    {
+      id: 2,
+      image: "/icons/gear_about.png",
+      text: "Transmission",
+      checkIcon: "/icons/check-orange.png",
+    },
+    {
+      id: 3,
+      image: "/icons/electric_about.png",
+      text: "Electrical",
+      checkIcon: "/icons/check-orange.png",
+    },
+    {
+      id: 4,
+      image: "/icons/cooling_about.png",
+      text: "Cooling",
+      checkIcon: "/icons/check-orange.png",
+    },
+    {
+      id: 5,
+      image: "/icons/more_about.png",
+      text: "20+ More",
+      checkIcon: "/icons/check-orange.png",
+    },
+  ];
+
   return (
-    <div className="container">
-      <div className="bloc-tabs">
-        <button
-          className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
-          onClick={() => toggleTab(1)}
-        >
-          Tab 1
-        </button>
-        <button
-          className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
-          onClick={() => toggleTab(2)}
-        >
-          Tab 2
-        </button>
-        <button
-          className={toggleState === 3 ? "tabs active-tabs" : "tabs"}
-          onClick={() => toggleTab(3)}
-        >
-          Tab 3
-        </button>
+    <div className="flex flex-col">
+      {/* -------------- Buttons --------------- */}
+      <div className="flex gap-6 justify-center">
+        {btnArray.map((btn, i) => (
+          <button
+            key={i}
+            onClick={handleClick}
+            className="btn glass font-medium text-gray-700 capitalize"
+          >
+            <Image
+              width={50}
+              height={50}
+              className="mr-2"
+              src={btn.image}
+              alt="cars"
+            />
+            {btn.text}
+            <Image
+              width={10}
+              height={10}
+              className="ml-2"
+              src={btn.checkIcon}
+              alt="cars"
+            />
+          </button>
+        ))}
       </div>
+      {/* ------------ Car details ------------ */}
+      <div className="flex flex-col my-[300px]">
+        <div className="relative">
+          {show ? (
+            <div className="flex flex-col">
+              {/* ------------ row -------------- */}
+              <div className="flex">
+                <div className="flex">
+                  <div className="absolute top-[-250px] flex flex-col items-center gap-4">
+                    <div className="w-[100px] h-[100px] flex items-center justify-center bg-white shadow-xl rounded-md">
+                      <Image
+                        width={30}
+                        height={30}
+                        className=""
+                        src="/about/price_icon.png"
+                        alt="cars"
+                      />
+                    </div>
+                    <p>Competitive Pricing</p>
+                  </div>
+                  <Image
+                    width={104}
+                    height={180}
+                    className="absolute top-[-190px] left-40"
+                    src="/about/top-left.png"
+                    alt="cars"
+                  />
+                </div>
 
-      <div className="content-tabs">
-        <div
-          className={toggleState === 1 ? "content  active-content" : "content"}
-        >
-          <h2>Content 1</h2>
-          <hr />
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
-            praesentium incidunt quia aspernatur quasi quidem facilis quo nihil
-            vel voluptatum?
-          </p>
+                <div className="flex">
+                  <div className="absolute top-[-250px] left-[400px] flex flex-col items-center gap-4">
+                    <div className="w-[100px] h-[100px] flex items-center justify-center bg-white shadow-xl rounded-md">
+                      <Image
+                        width={35}
+                        height={35}
+                        className=""
+                        src="/about/idea_icon.png"
+                        alt="cars"
+                      />
+                    </div>
+                    <p>Easier Rent</p>
+                    <p className="mt-[-20px]">On Your Budget</p>
+                  </div>
+                  <Image
+                    width={21}
+                    height={73}
+                    className="absolute top-[-82px] left-[450px]"
+                    src="/about/top-middle.png"
+                    alt="cars"
+                  />
+                </div>
+
+                <div className="flex">
+                  <div className="absolute top-[-250px] right-10 flex flex-col items-center gap-4">
+                    <div className="w-[100px] h-[100px] flex items-center justify-center bg-white shadow-xl rounded-md">
+                      <Image
+                        width={30}
+                        height={30}
+                        className=""
+                        src="/about/wallet_icon.png"
+                        alt="cars"
+                      />
+                    </div>
+                    <p>Most Flexible</p>
+                    <p className="mt-[-20px]">Payment Plan</p>
+                  </div>
+                  <Image
+                    width={104}
+                    height={180}
+                    className="absolute top-[-190px] right-[200px]"
+                    src="/about/top-right.png"
+                    alt="cars"
+                  />
+                </div>
+              </div>
+              {/* ------------ row ------------- */}
+              <div className="flex">
+                <div className="absolute top-[520px] flex flex-col items-center gap-4">
+                  <div className="w-[100px] h-[100px] flex items-center justify-center bg-white shadow-xl rounded-md">
+                    <Image
+                      width={35}
+                      height={35}
+                      className=""
+                      src="/about/budget_icon.png"
+                      alt="cars"
+                    />
+                  </div>
+                  <p>The Best Extended</p>
+                  <p className="mt-[-20px]">Auto Warrenties</p>
+                </div>
+                <div className="absolute top-[520px] left-[400px] flex flex-col items-center gap-4">
+                  <div className="w-[100px] h-[100px] flex items-center justify-center bg-white shadow-xl rounded-md">
+                    <Image
+                      width={35}
+                      height={35}
+                      className=""
+                      src="/about/assist_icon.png"
+                      alt="cars"
+                    />
+                  </div>
+                  <p>Roadside</p>
+                  <p className="mt-[-20px]">Assistance 24/7</p>
+                </div>
+                <div className="absolute top-[520px] right-10 flex flex-col items-center gap-4">
+                  <div className="w-[100px] h-[100px] flex items-center justify-center bg-white shadow-xl rounded-md">
+                    <Image
+                      width={35}
+                      height={35}
+                      className=""
+                      src="/about/service_icon.png"
+                      alt="cars"
+                    />
+                  </div>
+                  <p>Your Choice</p>
+                  <p className="mt-[-20px]">of Mechanic</p>
+                </div>
+              </div>
+            </div>
+          ) : null}
         </div>
 
-        <div
-          className={toggleState === 2 ? "content  active-content" : "content"}
-        >
-          <h2>Content 2</h2>
-          <hr />
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
-            voluptatum qui adipisci.
-          </p>
-        </div>
-
-        <div
-          className={toggleState === 3 ? "content  active-content" : "content"}
-        >
-          <h2>Content 3</h2>
-          <hr />
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos sed
-            nostrum rerum laudantium totam unde adipisci incidunt modi alias!
-            Accusamus in quia odit aspernatur provident et ad vel distinctio
-            recusandae totam quidem repudiandae omnis veritatis nostrum
-            laboriosam architecto optio rem, dignissimos voluptatum beatae
-            aperiam voluptatem atque. Beatae rerum dolores sunt.
-          </p>
-        </div>
+        <Image
+          width={864}
+          height={461}
+          src="/cars/car_top.png"
+          className="object-contain w-full h-full"
+          alt="car"
+        />
       </div>
     </div>
   );
---
+++ client/web/src/pages/about.tsx
@@ -0,0 +1,101 @@
+import DownloadApp from "@/components/Home/DownloadApp";
+import ScrollToTop from "@/components/Layout/ScrollToTop";
+import Tabs from "@/components/Tabs/Tabs";
+import Image from "next/image";
+
+const Account = () => {
+  return (
+    <div>
+      <Image
+        width={1920}
+        height={500}
+        className="w-full h-full]"
+        src="/cars/cars_about.png"
+        alt="cars"
+      />
+      <h1 className="text-2xl font-medium">Our Team</h1>
+      <div className="flex flex-col items-center">
+        <div className="flex justify-between w-11/12 items-center">
+          <div className="flex flex-col items-center">
+            <Image
+              width={163}
+              height={255}
+              className="hover:scale-95 duration-200"
+              src="/about/ganzo.png"
+              alt="ganzo"
+            />
+            <span className="mt-5">Ganzorig</span>
+            <p className="text-xs">ganzorig@gmail.com</p>
+          </div>
+          <div className="flex flex-col items-center mt-6">
+            <Image
+              width={163}
+              height={255}
+              className="hover:scale-95 duration-200"
+              src="/about/maira.png"
+              alt="maira"
+            />
+            <span className="mt-6">Maira</span>
+            <p className="text-xs">Maira@gmail.com</p>
+          </div>
+          <div className="flex flex-col items-center mt-10">
+            <Image
+              width={181}
+              height={242}
+              className="hover:scale-95 duration-200"
+              src="/about/temka.png"
+              alt="temka"
+            />
+            <span className="mt-5">Temuujin</span>
+            <p className="text-xs">temkasobri@gmail.com</p>
+          </div>
+          <div className="flex flex-col items-center mt-8">
+            <Image
+              width={172}
+              height={255}
+              className="hover:scale-95 duration-200"
+              src="/about/lkhagva.png"
+              alt="lkhagva"
+            />
+            <span className="mt-5">Lkhagva</span>
+            <p className="text-xs">Lkhagva@gmail.com</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <Image
+              width={170}
+              height={274}
+              className="hover:scale-95 duration-200"
+              src="/about/bilguun1.png"
+              alt="bilguun"
+            />
+            <span className="mt-6">Bilguun</span>
+            <p className="text-xs">Bilguun@gmail.com</p>
+          </div>
+        </div>
+        <div className="w-full mt-20 flex flex-col items-center">
+          <div className="w-full h-[1.5px] bg-gray-300"></div>
+          <h2 className="mt-10 text-xl font-semibold">About Us</h2>
+          <p className="mt-10 p-4 text-xs tracking-wide leading-6">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
+        </div>
+        <div className="flex flex-col items-center">
+          <h1 className="m-10 font-medium  text-red-primary">Top Rated Car Rental</h1>
+          <Tabs />
+        </div>
+      </div>
+      <DownloadApp />
+      <ScrollToTop />
+    </div>
+  );
+};
+
+export default Account;
